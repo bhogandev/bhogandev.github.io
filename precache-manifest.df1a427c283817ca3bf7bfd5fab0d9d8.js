2cdc162249fa84eae1a396800634b19d7255078f
Brandon Hogan <bhogandev@outlook.com>
2020-09-19 15:31:49 -0400

    Updates

---
+++ precache-manifest.df1a427c283817ca3bf7bfd5fab0d9d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a1ab23c11a55d620bcb614decf52d2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b010095167b6311649e",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "031c72a4326e61a5d94e",
+    "url": "/static/css/main.dee29415.chunk.css"
+  },
+  {
+    "revision": "2b010095167b6311649e",
+    "url": "/static/js/2.9e160746.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9e160746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "031c72a4326e61a5d94e",
+    "url": "/static/js/main.bc9cebaa.chunk.js"
+  },
+  {
+    "revision": "f3080afb5ec6c15a3554",
+    "url": "/static/js/runtime-main.5edfeb0b.js"
+  },
+  {
+    "revision": "06c45f44c07517f7da9e631357a78acc",
+    "url": "/static/media/portfolioavatar.06c45f44.JPEG"
+  }
+]);
