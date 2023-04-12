732e727cf727c76660bef054a38459bafd5eab73
Brandon Hogan <bhogandev@outlook.com>
2023-04-12 10:57:27 -0400

    uPDATE

--- bhogandev.com/src/middleware/githubAPI.js
+++ bhogandev.com/src/middleware/githubAPI.js
@@ -1,11 +1,10 @@
 const { Octokit } = require("@octokit/rest");
 
 const octokit = new Octokit({
-  auth: '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'
 });
 
 export default async function retrieveRepos(){
 
-return octokit.rest.repos.listForAuthenticatedUser();
+return octokit.rest.repos.listForUser({username: 'bhogandev'});
 }
 
